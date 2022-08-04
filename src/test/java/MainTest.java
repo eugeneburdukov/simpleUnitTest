8c1304ba239e8e5450b28775ae96926763d6188a
eugeneburdukov <eugenebkv@yahoo.com.ua>
2022-08-04 19:13:06 +0300

    third commit

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,13 +1,27 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class MainTest{
+
+    @Test
+    public void concatTest() {
+        String result = "eugene " + "burdukov";
+        assertEquals("eugene burdukov", result);
+    }
+
+    @Test
+    public void additionTest() {
+        int x = 10, y = 9, result;
+        result = x + y;
+        assertEquals(20, result);
+    }
 
     @Test
     public void isAgeEqual(){
         Student student1 = new Student("eugene", 29);
         Student student2 = new Student("alex", 29);
-
         Assert.assertTrue(student1.age == student2.age);
     }
 
@@ -15,15 +29,13 @@ public class MainTest{
     public void isNameEqual(){
         Student student1 = new Student("nikita", 31);
         Student student2 = new Student("nikita", 31);
-
-        Assert.assertTrue(student1.name == student2.name);
+        Assert.assertTrue(student1.name.equals(student2.name));
     }
 
     @Test
     public void areStudentsEqual(){
         Student student1 = new Student("nikita", 31);
         Student student2 = new Student("nikita", 31);
-
         Assert.assertTrue(student1.equals(student2));
     }
 
